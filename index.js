3b28803c97e0045ab6488c4bd11661f5525927ea
yangyuan <1367511704@qq.com>
2019-07-31 18:26:12 +0800

    fix(一些bug)

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-const request = require("request");
-const fs = require("fs");
-const staticPath = "ajax";
-const utils = require("./utils");
-const isRef = function (key) {
+const request = require('request');
+const fs = require('fs');
+const staticPath = 'ajax';
+const utils = require('./utils');
+const isRef = function(key) {
   if (!key || key !== '$ref') {
     return;
   } else {
-    isRef(key)
+    isRef(key);
   }
-}
+};
 const createControllerJs = function(res) {
   const data = res.map(item => {
     item.name = utils.tranformStr(item.name);
@@ -22,12 +22,8 @@ const createUrl = function(params) {
   for (const key in params) {
     const obj = {
       key,
-      controller:
-        params[key].post && params[key].post.tags[0]
-          ? utils.tranformStr(params[key].post.tags[0])
-          : "",
-      description:params[key].post && params[key].post.description ? params[key].post.description : '请后台添加接口说明'
-
+      controller: params[key].post && params[key].post.tags[0] ? utils.tranformStr(params[key].post.tags[0]) : '',
+      description: params[key].post && params[key].post.description ? params[key].post.description : '请后台添加接口说明'
     };
     urls.push(obj);
   }
@@ -38,9 +34,7 @@ const mergeUrl = function(data) {
   const hash = {};
   const res = [];
   data.forEach(function(item) {
-    !hash[item.controller]
-      ? (hash[item.controller] = [item])
-      : hash[item.controller].push(item);
+    !hash[item.controller] ? (hash[item.controller] = [item]) : hash[item.controller].push(item);
   });
   for (const key in hash) {
     res.push({
@@ -50,48 +44,55 @@ const mergeUrl = function(data) {
   }
   return res;
 };
-const annotationTemp = function (params) {
-  const content =
-  `/** 
+const annotationTemp = function(params) {
+  const content = `/** 
   ${params.description}
    */`;
-  return content
-}
-const FunContent = function (funName) {
+  return content;
+};
+const FunContent = function(funName, type) {
   // const contents = [];
   const content = `function ${funName}() {
-    
+    fetchResource.post({type:'${type + funName}'})
   }`;
-  return content
-}
-const contentJs = function (data, key) {    
-    let content = '';
-    data.forEach(item => {
-        if (item.controller === key) {
-          console.log(item)
-          content = `const baseUrl = '${item.urls[0].key.substr(0, item.urls[0].key.lastIndexOf('/')+1)}';\n`
-            // item && item.urls ? (content = FunContent(item.controller)) : (content = '')
-            if (item && item.urls) {
-              item.urls.forEach(item => {
-                let str = item.key.toString()
-                content += `${annotationTemp(item)} \n ${FunContent(str.substr(str.lastIndexOf('/')+1))} \n`;
-              })
-            }
-        };
-    })
-    return content
-}
-request("http://192.168.1.43:9101/v2/api-docs", (error, response, res) => {
+  return content;
+};
+const contentJs = function(data, key) {
+  let content = '';
+  const funcName = [];
+  let type = '';
+  data.forEach(item => {
+    if (item.controller === key) {
+      console.log(item);
+      content = "import { fetchResource } from './fetchapi';\n\n";
+      type = '';
+      type = item.urls[0].key.substr(0, item.urls[0].key.lastIndexOf('/') + 1);
+      content += `const baseUrl = '${type}';\n`;
+      // item && item.urls ? (content = FunContent(item.controller)) : (content = '')
+      if (item && item.urls) {
+        item.urls.forEach(item => {
+          let str = item.key.toString();
+          funcName.push(str.substr(str.lastIndexOf('/') + 1));
+          content += `${annotationTemp(item)} \n ${FunContent(str.substr(str.lastIndexOf('/') + 1), type)} \n`;
+        });
+      }
+      content += `export { ${funcName.join(',')} }`;
+    }
+  });
+  return content;
+};
+request('http://101.231.154.154:18050/v2/api-docs', (error, response, res) => {
   const data = JSON.parse(res);
   const paths = data.paths;
   const createUrlData = createUrl(paths);
   const mergeUrlData = mergeUrl(createUrlData);
-  createControllerJs(data.tags).forEach(item => { // 创建conTroller.js
-      fs.writeFileSync(`${staticPath}/${item.name}.js`, `/**${item.description}*/\n${contentJs(mergeUrlData,item.name)}`, err => {
-          if (err) {
-              throw err
-          }
-          console.log(`${item.name}写入成功`)
-      })
+  createControllerJs(data.tags).forEach(item => {
+    // 创建conTroller.js
+    fs.writeFileSync(`${staticPath}/${item.name}.js`, `/**${item.description}*/\n${contentJs(mergeUrlData, item.name)}`, err => {
+      if (err) {
+        throw err;
+      }
+      console.log(`${item.name}写入成功`);
+    });
   });
 });
