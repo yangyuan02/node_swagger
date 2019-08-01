c25751d0f5e07448bb957490219ed176fdf095bc
yangyuan <1367511704@qq.com>
2019-08-01 13:27:14 +0800

    fix(修改生成代码注释)

--- index.js
+++ index.js
@@ -16,14 +16,37 @@ const createControllerJs = function(res) {
   });
   return data;
 };
-
+const schemaFun = function(params, key) {
+  let str = '';
+  if (
+    params[key].post &&
+    params[key].post.parameters &&
+    params[key].post.parameters[0] &&
+    params[key].post.parameters[0].schema &&
+    params[key].post.parameters[0].schema['$ref']
+  ) {
+    str = params[key].post.parameters[0].schema['$ref'].substr(14);
+  }
+  if (
+    params[key].post &&
+    params[key].post.parameters &&
+    params[key].post.parameters[0] &&
+    params[key].post.parameters[0].schema &&
+    params[key].post.parameters[0].schema['items'] &&
+    params[key].post.parameters[0].schema['items']['$ref']
+  ) {
+    str = params[key].post.parameters[0].schema['items']['$ref'].substr(14);
+  }
+  return str;
+};
 const createUrl = function(params) {
   const urls = [];
   for (const key in params) {
     const obj = {
       key,
       controller: params[key].post && params[key].post.tags[0] ? utils.tranformStr(params[key].post.tags[0]) : '',
-      description: params[key].post && params[key].post.description ? params[key].post.description : '请后台添加接口说明'
+      description: params[key].post && params[key].post.description ? params[key].post.description : '请后台添加接口说明',
+      schema: schemaFun(params, key)
     };
     urls.push(obj);
   }
@@ -44,36 +67,36 @@ const mergeUrl = function(data) {
   }
   return res;
 };
-const annotationTemp = function(params) {
+const annotationTemp = function(params, comments) {
   const content = `/** 
   ${params.description}
+    ${JSON.stringify(comments.properties, null, 4)}
    */`;
   return content;
 };
 const FunContent = function(funName, type) {
   // const contents = [];
   const content = `function ${funName}() {
-    fetchResource.post({type:'${type + funName}'})
+    return fetchResource.post({type:'${type + funName}'}).then()
   }`;
   return content;
 };
-const contentJs = function(data, key) {
+const contentJs = function(data, key, definitions) {
   let content = '';
   const funcName = [];
   let type = '';
   data.forEach(item => {
     if (item.controller === key) {
-      console.log(item);
       content = "import { fetchResource } from './fetchapi';\n\n";
       type = '';
       type = item.urls[0].key.substr(0, item.urls[0].key.lastIndexOf('/') + 1);
       content += `const baseUrl = '${type}';\n`;
-      // item && item.urls ? (content = FunContent(item.controller)) : (content = '')
       if (item && item.urls) {
         item.urls.forEach(item => {
           let str = item.key.toString();
           funcName.push(str.substr(str.lastIndexOf('/') + 1));
-          content += `${annotationTemp(item)} \n ${FunContent(str.substr(str.lastIndexOf('/') + 1), type)} \n`;
+          let comments = definitions[item.schema] ? definitions[item.schema] : {};
+          content += `${annotationTemp(item, comments)} \n ${FunContent(str.substr(str.lastIndexOf('/') + 1), type)} \n`;
         });
       }
       content += `export { ${funcName.join(',')} }`;
@@ -84,15 +107,20 @@ const contentJs = function(data, key) {
 request('http://101.231.154.154:18050/v2/api-docs', (error, response, res) => {
   const data = JSON.parse(res);
   const paths = data.paths;
+  const definitions = data.definitions;
   const createUrlData = createUrl(paths);
   const mergeUrlData = mergeUrl(createUrlData);
   createControllerJs(data.tags).forEach(item => {
     // 创建conTroller.js
-    fs.writeFileSync(`${staticPath}/${item.name}.js`, `/**${item.description}*/\n${contentJs(mergeUrlData, item.name)}`, err => {
-      if (err) {
-        throw err;
+    fs.writeFileSync(
+      `${staticPath}/${item.name}.js`,
+      `/**${item.description}*/\n${contentJs(mergeUrlData, item.name, definitions)}`,
+      err => {
+        if (err) {
+          throw err;
+        }
+        console.log(`${item.name}写入成功`);
       }
-      console.log(`${item.name}写入成功`);
-    });
+    );
   });
 });
